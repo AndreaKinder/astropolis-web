0dcfe8d1a36d1203ef7c87c89272c1a6ec5a6088
AndreaKinder <andvillart@gmail.com>
2024-05-26 13:20:18 +0200

    Init page

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'ASTRóPOLIS';
+export const SITE_DESCRIPTION = 'Análisis Astrológicos';
